bf254fdf8132b90b13230b622893cf7cdf6b94c2
AbedDoesCode <98119874+AbdelrahmanAhmed605@users.noreply.github.com>
2023-02-01 21:00:53 -0800

    Added feature to take user input and fetch the gnews api with our user input passed as a parameter to retrieve the news article data returned by the api

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,28 @@ function userFormSubmit(event) {
 
   const topic = userTopicSelect.val();
   if (topic) {
-    getNewsArticles(topic);
+    getNewsData(topic);
     topic.val("");
   } else {
     alert("Please enter a topic");
   }
+}
+
+function getNewsData(topic) {
+  const apikey = "0a81fd50979ee58ec90f9d378ec0e3ef";
+  const newsurl = `https://gnews.io/api/v4/search?q=${topic}&token=${apikey}&lang=en&country=us&max=10`;
+
+  fetch(newsurl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          getNewsArticles(data);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert("Error:", error);
+    });
 }
