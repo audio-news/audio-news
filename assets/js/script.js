08a4266ed8e39750c92c7e684c8ca3886f0a6c29
AbedDoesCode <98119874+AbdelrahmanAhmed605@users.noreply.github.com>
2023-02-01 21:14:58 -0800

    Added feature to extract desired data (article title, description, and image) from the JSON data returned by the fetch call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 var userTopicForm = $();
 var userTopicSelect = $();
 
+/* Checks to see if user placed an entry when submitting the form. If an entry was 
+placed, then call the getNewsData function to fetch the data with gnews API */
 function userFormSubmit(event) {
   event.preventDefault();
 
@@ -13,6 +15,9 @@ function userFormSubmit(event) {
   }
 }
 
+/* Fetches the article data using the gnews API. Checks to see if the fetch promise is returned.
+If not it catches the error. Once the fetch promise is retrieved, check the status code of the returned data.
+If the status is ok, calls getNewsArticles function to extract desired data*/
 function getNewsData(topic) {
   const apikey = "0a81fd50979ee58ec90f9d378ec0e3ef";
   const newsurl = `https://gnews.io/api/v4/search?q=${topic}&token=${apikey}&lang=en&country=us&max=10`;
@@ -30,4 +35,15 @@ function getNewsData(topic) {
     .catch(function (error) {
       alert("Error:", error);
     });
+}
+
+/* Extracts the desired data (article titles, description, and image) */
+function getNewsArticles(data) {
+    console.log(data);
+    var articles = data.articles;
+    for (i = 0; i < articles.length; i++) {
+        var articleTitle = articles[i]["title"];
+        var articleDesc = articles[i]["description"];
+        var articleImgUrl = articles[i]["image"];
+    }
 }
